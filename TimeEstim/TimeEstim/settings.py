7b1d27981adfdf5bca4616eb09ec81851067f9fc
Marina Krivtsoun <marinaktsn@gmail.com>
2021-04-15 22:23:59 +0300

    static variables fixed

--- TimeEstim/TimeEstim/settings.py
+++ TimeEstim/TimeEstim/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Configure Django App for Heroku.
