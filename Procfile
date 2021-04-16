cecac0dec8d4e01d721c98bfb1c379056dacecc0
Marina Krivtsoun <marinaktsn@gmail.com>
2021-04-16 20:01:38 +0300

    amended access in Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate --no-input
+release: python TimeEstim/manage.py migrate --no-input
 web: gunicorn --chdir TimeEstim TimeEstim.wsgi:application --log-file -
--- TimeEstim/apps/core/models.py
+++ TimeEstim/apps/core/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Userprofile(models.Model):
     user = models.OneToOneField(User, related_name='uprofile', on_delete=models.CASCADE)
-    velocity = models.IntegerField(default=0)
+    # velocity = models.IntegerField(default=0)
